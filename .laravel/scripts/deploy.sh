aeb94fa66de43686428a56e1985a75f08b6a1cc1
romeroa14 <alfredoromerox15@gmail.com>
2025-07-06 14:01:07 -0400

    Agregar soporte para directorio .laravel/cache

--- .laravel/scripts/deploy.sh
+++ .laravel/scripts/deploy.sh
@@ -4,12 +4,15 @@
 mkdir -p storage/app/public/products
 mkdir -p storage/framework/{sessions,views,cache}
 mkdir -p storage/logs
+mkdir -p .laravel/cache
 
 # Establecer permisos
 chmod -R 775 storage
 chmod -R 775 bootstrap/cache
+chmod -R 775 .laravel/cache
 chown -R www-data:www-data storage
 chown -R www-data:www-data bootstrap/cache
+chown -R www-data:www-data .laravel/cache
 
 # Crear enlace simbólico
 php artisan storage:link
