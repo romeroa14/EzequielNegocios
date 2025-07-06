d4d1797bf9592e696155d8f96c06b57ed185b17d
romeroa14 <alfredoromerox15@gmail.com>
2025-07-06 14:41:49 -0400

    Actualizar configuración de R2 para usar credenciales de Laravel Cloud

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,6 +47,22 @@ return [
             'report' => false,
         ],
 
+        'r2' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION', 'auto'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => true,
+            'report' => true,
+            'visibility' => 'public',
+            'bucket_endpoint' => false,
+            'scheme' => 'https',
+        ],
+
         // 's3' => [
         //     'driver' => 's3',
         //     'key' => env('AWS_ACCESS_KEY_ID'),
